5f0257d190d086d7a1e6a99e00d7fc5527140fe7
Sankalp <sankalp.kulkarni05@gmail.com>
2023-02-14 08:26:23 +0530

    [Sankalp]- Ability to calculate cab fare

--- CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace CabInvoiceGenerator
+﻿using System.IO;
+
+namespace CabInvoiceGenerator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Cab Invoice Generator Problem!!");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine("Total fare : " + fare);
         }
     }
 }
---
+++ CabInvoiceGenerator/CabInvoiceTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using CabInvoiceGenerator;
+
+namespace CabInvoiceTest
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+        [SetUp]
+        public void Setup()
+        {
+        }
+        // Test Case for UC1-Calculate Total Fare
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
